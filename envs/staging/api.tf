a4750f270e9aad0b3b62efe1421f2a4e238a9656
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-03-26 15:29:11 +0100

    chore(staging): bump version of db-migrations

--- envs/staging/api.tf
+++ envs/staging/api.tf
@@ -3,7 +3,7 @@ locals {
     image_tags = {
       database_layer   = "0.3.78"
       server           = "staging"
-      api_db_migration = "0.19.0-staging.0"
+      api_db_migration = "0.19.0-staging.1"
     }
   }
 }
