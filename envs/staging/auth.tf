17ed0308bde93eed75f4a7f2defc5e7dcf5e3023
Hugo Tiburtino <hugotiburtino@protonmail.com>
2024-05-09 00:11:29 +0200

    feat(auth): remove hydra from staging since it isn't used anymore

--- envs/staging/auth.tf
+++ envs/staging/auth.tf
@@ -1,28 +1,9 @@
 locals {
   ory_chart_version = "0.23.3"
 
-  hydra = {
-    image_tag = "v1.11.8"
-  }
-
   kratos = {
     image_tag = "v1.0.0"
   }
-}
-
-module "hydra" {
-  source = "../../modules/hydra"
-
-  namespace     = kubernetes_namespace.auth_namespace.metadata.0.name
-  chart_version = local.ory_chart_version
-  image_tag     = local.hydra.image_tag
-  node_pool     = module.cluster.node_pools.preemptible
-
-  dsn         = "postgres://${var.postgres_database_username_default}:${var.kpi_kpi_database_password_default}@${module.gcloud_postgres.database_private_ip_address}/hydra"
-  url_login   = "https://${local.domain}/auth/oauth/login"
-  url_logout  = "https://${local.domain}/auth/oauth/logout"
-  url_consent = "https://${local.domain}/auth/oauth/consent"
-  host        = "hydra.${local.domain}"
 }
 
 module "kratos" {
