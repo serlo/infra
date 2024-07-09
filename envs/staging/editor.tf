f906e68554875bf5b00b1915fe462839bdbde3fa
Vitomir Budimir <budimirvitomir@gmail.com>
2024-07-09 10:34:07 +0200

    feat(editor): add mysql database url env var for lti integration

--- envs/staging/editor.tf
+++ envs/staging/editor.tf
@@ -6,6 +6,8 @@ module "editor" {
 
   dev_mode = true
 
+  mysql_database_url = "mysql://serlo:${var.athene2_database_password_default}@${module.mysql.database_private_ip_address}:3306/serlo"
+
   lti_platform_client_id = var.editor_lti_platform_client_id
 }
 
