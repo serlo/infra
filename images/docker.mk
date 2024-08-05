b4c05864bcdef8f43d33cbeea0cb58f18a6cceac
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-08-05 15:21:37 +0200

    style: remove unnecessary comments

--- images/docker.mk
+++ images/docker.mk
@@ -27,12 +27,10 @@ gce_image := eu.gcr.io/serlo-shared/$(image_name)
 patch_version ?= $(shell git log --pretty=format:'' | wc -l)
 
 .PHONY: docker_build_push
-# builds the docker image in the ci and pushes it to eu.gcr.io
 docker_build_push:
 	 docker pull $(gce_image):$(version) 2>/dev/null >/dev/null || $(MAKE) docker_build docker_push
 
 .PHONY: docker_push
-# push docker container to gcr.io registry
 docker_push:
 	docker tag $(local_image):latest $(gce_image):latest
 	docker push $(gce_image):latest
@@ -44,5 +42,3 @@ docker_push:
 	docker push $(gce_image):$(version)
 	docker tag $(local_image):latest $(gce_image):sha-$(shell git describe --dirty --always)
 	docker push $(gce_image):sha-$(shell git describe --dirty --always)
-
-
