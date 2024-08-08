e5d55e32c71651ec8c29a46105ea5663690a1f1e
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-08-08 09:00:11 +0200

    format: run make `terraform_fmt`

--- envs/ionos-test/main.tf
+++ envs/ionos-test/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     ionoscloud = {
-      source = "ionos-cloud/ionoscloud"
+      source  = "ionos-cloud/ionoscloud"
       version = "6.5.0"
     }
   }
@@ -12,29 +12,29 @@ provider "ionoscloud" {
 }
 
 resource "ionoscloud_datacenter" "serlo_datacenter" {
-  name                    = "serlo_datacenter"
-  location                = "de/txl"
+  name     = "serlo_datacenter"
+  location = "de/txl"
 }
 
-resource "ionoscloud_lan"  "serlo_lan" {
-  datacenter_id           = ionoscloud_datacenter.serlo_datacenter.id
-  public                  = false
-  name                    = "serlo_lan"
+resource "ionoscloud_lan" "serlo_lan" {
+  datacenter_id = ionoscloud_datacenter.serlo_datacenter.id
+  public        = false
+  name          = "serlo_lan"
 }
 
-resource ionoscloud_mongo_cluster "serlo_mongo_cluster" {
+resource "ionoscloud_mongo_cluster" "serlo_mongo_cluster" {
   maintenance_window {
-    day_of_the_week  = "Sunday"
-  time             = "09:00:00"
+    day_of_the_week = "Sunday"
+    time            = "09:00:00"
   }
   mongodb_version = "5.0"
-  instances          = 1
-  display_name = "serlo_mongo_cluster"
-  location = ionoscloud_datacenter.serlo_datacenter.location
-  connections   {
-    datacenter_id   =  ionoscloud_datacenter.serlo_datacenter.id
-    lan_id          =  ionoscloud_lan.serlo_lan.id
-    cidr_list            =  ["192.168.1.108/24"]
+  instances       = 1
+  display_name    = "serlo_mongo_cluster"
+  location        = ionoscloud_datacenter.serlo_datacenter.location
+  connections {
+    datacenter_id = ionoscloud_datacenter.serlo_datacenter.id
+    lan_id        = ionoscloud_lan.serlo_lan.id
+    cidr_list     = ["192.168.1.108/24"]
   }
   template_id = "6b78ea06-ee0e-4689-998c-fc9c46e781f6"
 }
