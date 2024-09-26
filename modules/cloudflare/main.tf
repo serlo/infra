993ba6da8f6ee7afba55da53eaeb0890628252d2
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-09-26 22:38:23 +0200

    remove dns editor to not be managed anymore by terraform

--- modules/cloudflare/main.tf
+++ modules/cloudflare/main.tf
@@ -123,12 +123,3 @@ resource "cloudflare_record" "cname_enmeshed" {
   type    = "CNAME"
   proxied = true
 }
-
-# editor (Serlo Editor as LTI Tool)
-resource "cloudflare_record" "cname_editor" {
-  zone_id = var.zone_id
-  name    = "editor"
-  value   = var.domain
-  type    = "CNAME"
-  proxied = true
-}
