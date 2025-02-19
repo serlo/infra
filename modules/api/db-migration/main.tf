83c039885728c3502703fadf413f1be58452e874
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-02-19 19:11:50 +0100

    refactor(db-migration): set resources to avoid error out of memory

--- modules/api/db-migration/main.tf
+++ modules/api/db-migration/main.tf
@@ -115,6 +115,16 @@ resource "kubernetes_cron_job_v1" "migration_cron_job" {
                   name = kubernetes_config_map.envvars.metadata.0.name
                 }
               }
+              resources {
+                limits = {
+                  cpu    = "1500m"
+                  memory = "1G"
+                }
+                requests = {
+                  cpu    = "575m"
+                  memory = "350M"
+                }
+              }
             }
 
             restart_policy = "Never"
