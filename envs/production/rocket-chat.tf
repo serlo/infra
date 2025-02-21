6978ed2be5e3e1641c4dd1bc7a6c854eb32ba0ab
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-02-21 02:07:53 +0100

    minor chart upgrade of rocket chat mongo

--- envs/production/rocket-chat.tf
+++ envs/production/rocket-chat.tf
@@ -2,7 +2,7 @@ locals {
   rocket_chat = {
     chart_versions = {
       rocketchat = "4.7.4"
-      mongodb    = "14.0.0"
+      mongodb    = "14.0.14"
     }
     image_tags = {
       rocketchat = "6.13.0"
